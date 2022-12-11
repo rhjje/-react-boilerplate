24aaa8b74e9790205a79585fa6821e67ecadd6fb
Alexandr Yanykin <al.yanykin@gmail.com>
2022-12-11 16:37:03 +0300

    feat: added paths resolver

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import path from 'path';
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -5,4 +7,9 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
